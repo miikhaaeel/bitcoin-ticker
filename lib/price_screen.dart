a2f95eff568bba90ce70581907284d4c12299f7c
Miikhaaeel <fadowomichael1@gmail.com>
2021-08-09 23:33:43 +0100

    refactored dropdown button

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +7,22 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedValue = 'USD';
+
+  List<DropdownMenuItem<String>> getDropDownItem() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+
+      dropDownItems.add(newItem);
+    }
+    return dropDownItems;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +59,15 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton(
+              value: selectedValue,
+              items: getDropDownItem(),
+              onChanged: (value) {
+                setState(() {
+                  selectedValue = value as String;
+                });
+              },
+            ),
           ),
         ],
       ),
